3eb0a7ba40d339887f5a974cf8a6a56f1f436be9
Tarang Patel <tarangp@me.com>
2014-07-17 16:25:28 +0200

    Update server params

--- lib_server.js
+++ lib_server.js
@@ -1,12 +1,12 @@
 //Server side hooks
 
-var tail_version = "0.3.9",
+var tail_version = "0.4.0",
     Frequency_key = "",
     tail_settings = new Meteor.Collection("tail_settings"),
     tail_setup = false,
     booted = new Date(),
     ignoreLoad = false,
-    Galactic_core = DDP.connect("https://tail.sh"),
+    Galactic_core = DDP.connect("http://tail.sh"),
     Tail_Server_Settings = new Meteor.Collection("tail_settings", { connection: Galactic_core });
 
 Providers = {};
